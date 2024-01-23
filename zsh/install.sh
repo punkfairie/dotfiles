a47125d6a864d36f4191a801bfc2796b6fbc2e4b
Marley Rae <marley@punkfairie.net>
2024-01-23 11:54:02 -0800

    ✨ Add Fira Code Nerd Font install

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,5 +4,6 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" \
   && . "../homebrew/brew_utils.sh"
 
+brew_install "FiraCode Nerd Font" "font-fira-code-nerd-font" "" "homebrew/cask-fonts"
 brew_install "Starship Prompt" "starship"
 brew_install "Zsh Syntax Highlighting" "zsh-syntax-highlighting"
