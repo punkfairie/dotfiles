8cc3e0afd2708f0b72d1052a2619494d2edcad8d
Marley Rae <marley@punkfairie.net>
2024-01-22 21:01:13 -0800

    ✨ Make Homebrew install stuff

--- git/install.sh
+++ git/install.sh
@@ -4,4 +4,6 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" \
   && . "../homebrew/brew_utils.sh"
 
-brew_install "Starship Prompt" "starship"
+brew_install "Git" "git"
+brew_install "Github CLI" "gh"
+brew_install "Gitmoji" "gitmoji"
