308432ccbbe64d62a754495238313d84f35f7eb6
Marley Rae <marley@punkfairie.net>
2024-01-29 17:26:52 -0800

    ♻️  Node_utils -> fish

---
+++ node/node_utils.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+source "$DOT/script/utils.fish"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+function npm_install -a $msg $pkg
+  execute \
+    "source $DOT/node/path.config.fish && npm install --global --silent $pkg" \
+    "$msg"
+end
