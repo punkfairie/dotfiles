2909b791d73c8cc99b97a1f9430d2f531162a725
Marley Rae <marley@punkfairie.net>
2024-01-29 17:10:08 -0800

    ✨ Install Git tools in fish.

---
+++ git/install.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+source "$DOT/homebrew/brew_utils.fish"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+brew_install "Github CLI" "gh"
+brew_install "Gitmoji" "gitmoji"
