4c3929e3c422c9c18b542b4b204865569d25cd04
Marley Rae <marley@punkfairie.net>
2024-01-30 19:42:15 -0800

    🧑‍💻 Fix fish-colored-man colors

--- fish/theme.config.fish
+++ fish/theme.config.fish
@@ -3,8 +3,8 @@
 # fish-colored-man
 set -g man_blink -o f38ba8 red
 set -g man_bold -o 94e2d5 cyan
-set -g man_standout -b 11111b black
-set -g man_underline -u 11111b black
+set -g man_standout -o a6e3a1 green
+set -g man_underline -u f5c2e7 magenta
 
 # Starship
 set -gx STARSHIP_CONFIG "$DOT/fish/starship.toml"
