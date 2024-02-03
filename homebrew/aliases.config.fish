ad7963c4afd1b2c6be43ef7701537cb989934350
Marley Rae <marley@punkfairie.net>
2024-02-02 19:06:05 -0800

    ✨ Add Homebrew aliases

---
+++ homebrew/aliases.config.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+abbr -a brewi --position command "brew install"
+abbr -a brewr --position command "brew uninstall"
+abbr -a brewu --position command "brew update && brew upgrade && brew cleanup"
