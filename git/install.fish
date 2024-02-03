101648b52b7461d46f9f28f7144dbb66d47b2681
Marley Rae <marley@punkfairie.net>
2024-02-03 12:13:00 -0800

    ✨ Add gitmoji-fuzzy-hook

--- git/install.fish
+++ git/install.fish
@@ -8,4 +8,7 @@ source "$DOT/homebrew/brew_utils.fish"
 print_subtitle Git
 
 brew_install "Github CLI" gh
-brew_install Gitmoji gitmoji
+execute \
+    "git clone https://gitlab.com/raabf/gitmoji-fuzzy-hook.git ~/.local/share/gitmoji-fuzzy-hook && \
+     ln -f -s ~/.local/share/gitmoji-fuzzy-hook/bin/gitmoji-fuzzy-hook-init.sh $HOME/bin/gitmoji-fuzzy-hook-init" \
+    gitmoji-fuzzy-hook
