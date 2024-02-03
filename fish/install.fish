c540615e6eaea4ebdfaec3618c5c61350eb54bb4
Marley Rae <marley@punkfairie.net>
2024-02-03 14:44:33 -0800

    🔥 Remove unneccesary zsh plugins

--- fish/install.fish
+++ fish/install.fish
@@ -13,7 +13,5 @@ execute "fisher install decors/fish-colored-man" fish-colored-man
 
 brew_install "FiraCode Nerd Font" font-fira-code-nerd-font "" homebrew/cask-fonts
 brew_install "Starship Prompt" starship
-brew_install "Zsh Syntax Highlighting" zsh-syntax-highlighting
-brew_install "Zsh Autosuggestions" zsh-autosuggestions
 brew_install "The Fuck" thefuck
 npm_install "tldr pages" tldr
