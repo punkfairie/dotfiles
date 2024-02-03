bbd2414ef63950f6b938216bd0a6a89ab7ec856c
Marley Rae <marley@punkfairie.net>
2024-02-02 19:09:51 -0800

    ✨ OS aliases

--- os/aliases.config.fish
+++ os/aliases.config.fish
@@ -5,5 +5,14 @@ if [ "$(uname)" = Darwin ]
     abbr -a o --position command open
 
 else if [ "$(uname)" = Linux ]
+    abbr -a afk --position command "gnome-screensaver-command --lock"
     abbr -a o --position command xdg-open
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # APT.
+
+    abbr -a apti --position command "sudo apt install"
+    abbr -a aptr --position command "sudo apt remove"
+    abbr -a aptu --position command "sudo apt update && sudo apt upgrade"
 end
