6ceb6b1e4206159acc363bb4a2a5b03a5519f2b2
Marley Rae <marley@punkfairie.net>
2024-02-04 16:03:06 -0800

    🐛 Make Node.js stop showing false failure

--- node/volta.fish
+++ node/volta.fish
@@ -8,4 +8,5 @@ print_title "Volta & Node.js"
 
 execute "curl https://get.volta.sh | bash -s -- --skip-setup" Volta
 
-execute ". $DOT/node/path.config.fish && volta install node" "Node.js LTS"
+execute "if ! cmd_exists 'volta'; source $DOT/node/path.config.fish; end && volta install node" \
+    "Node.js LTS"
