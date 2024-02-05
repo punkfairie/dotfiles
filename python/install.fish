76ee3cd464c7f58ab9f8ace9edc787d50b8551db
Marley Rae <marley@punkfairie.net>
2024-02-04 17:29:44 -0800

    ✨ Use newer, Homebrew managed Python & Ruby

---
+++ python/install.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+source "$DOT/script/utils.fish"
+source "$DOT/homebrew/brew_utils.fish"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_subtitle Python
+
+brew_install Python python
---
+++ ruby/install.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+source "$DOT/script/utils.fish"
+source "$DOT/homebrew/brew_utils.fish"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_subtitle Ruby
+
+brew_install Ruby ruby
