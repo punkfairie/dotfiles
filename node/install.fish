51c2c1095169010b1272250b4e8e3ba2b47a9a91
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-02-10 19:25:09 -0800

    ✨ Add npm-fzf

---
+++ node/install.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+source "$DOT/script/utils.fish"
+source "$DOT/node/node_utils.fish"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_subtitle "Node & npm"
+
+npm_install npm-fzf npm-fzf
