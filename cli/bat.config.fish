66e47514f35cd3121a46cf80f3224eba47069755
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-02-10 17:09:21 -0800

    🐛 Fix bat config path

--- cli/bat.config.fish
+++ cli/bat.config.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
-set -gx BAT_CONFIG_PATH "$DOT/cli-tools/bat/bat.conf"
-set -gx BAT_CONFIG_DIR "$DOT/cli-tools/bat"
+set -gx BAT_CONFIG_PATH "$DOT/cli/bat/bat.conf"
+set -gx BAT_CONFIG_DIR "$DOT/cli/bat"
 
 function cat --wraps cat
     bat $argv
