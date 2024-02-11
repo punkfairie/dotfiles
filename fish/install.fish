9692a6a7eb04096ac365ec458c3563c96abe6205
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-02-10 16:00:38 -0800

    🚚 Finish reorganizing

--- fish/install.fish
+++ fish/install.fish
@@ -8,21 +8,5 @@ source "$DOT/node/node_utils.fish"
 
 print_subtitle Fish
 
-brew_install "GNU Core Utilities" coreutils
-execute "ln -sf '$(brew --prefix)/bin/gsha256sum' '$(brew --prefix)/bin/sha256sum'" \
-    "Use brew installed GNU Core Utils by default"
-
-brew_install wget wget --with-iri
-brew_install cURL curl
-brew_install grep grep
-brew_install OpenSSH openssh
-brew_install ImageMagick imagemagick
-brew_install ssh-copy-id ssh-copy-id
-
 execute "fisher install catppuccin/fish" catppuccin/fish
 execute "fisher install decors/fish-colored-man" fish-colored-man
-
-brew_install "FiraCode Nerd Font" font-fira-code-nerd-font "" homebrew/cask-fonts
-brew_install "Starship Prompt" starship
-brew_install "The Fuck" thefuck
-brew_install eza eza
