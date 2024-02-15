791e962b9c9ba87dc729649fab845455b098d3d2
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-02-14 21:09:46 -0800

    💄 fix: Change Wezterm font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,32 @@
 -- wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = "Catppuccin Macchiato"
 config.enable_tab_bar = false
 
-config.default_prog = { '/usr/bin/fish' }
+config.default_prog = { "/usr/bin/fish" }
 
-config.font = wezterm.font {
-	family = 'FiraCode Nerd Font',
-	harfbuzz_features = { 'cv02', 'cv06', 'ss01', 'cv14', 'onum', 'ss04', 'cv18', 'cv31', 'cv30', 'cv25', 'cv26', 'cv32', 'ss06', 'ss07' },
-}
+config.font = wezterm.font({
+	family = "FiraCode Nerd Font",
+	harfbuzz_features = {
+		"cv02",
+		"cv06",
+		"ss01",
+		"cv14",
+		"onum",
+		"ss04",
+		"cv18",
+		"cv31",
+		"cv30",
+		"cv25",
+		"cv26",
+		"cv32",
+		"ss06",
+		"ss07",
+	},
+})
+config.font_size = 11.0
 
 return config
