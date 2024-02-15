0420b8265f64406285fa69adde89f2a299abc4bc
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-02-14 21:08:45 -0800

    🐟 chore: Pull in fish configs

---
+++ .config/fish/conf.d/10-fish-colored-man.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+set -g man_blink -o f38ba8 red
+set -g man_bold -o 94e2d5 cyan
+set -g man_standout -o a6e3a1 green
+set -g man_underline -u f5c2e7 magenta
---
+++ .config/fish/conf.d/20-aliases.fish
@@ -0,0 +1,64 @@
+#!/usr/bin/env fish
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Shell aliases.
+abbr -a rl --position command "source ~/.config/fish/config.fish"
+abbr -a c --position command clear
+abbr -a e --position command "$EDITOR"
+abbr -a v --position command "$EDITOR"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Location aliases.
+abbr -a hack --position command "cd $HACK"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Default command options.
+
+abbr -a cp --position command "cp -iv"
+#                                  │└─ list copied files
+#                                  └─ prompt before overwriting an existing file
+
+abbr -a mkdir --position command "mkdir -pv"
+#                                        │└─ list created directories
+#                                        └─ create intermediate directories
+
+abbr -a mv --position command "mv -iv"
+#                                  │└─ list moved files
+#                                  └─ prompt before overwriting an existing file
+
+abbr -a rm --position command "rm -rf"
+#                                  │└─ do not ask for confirmation
+#                                  └─ recursively remove directories and files
+
+# Colored grep output.
+abbr -a grep --position command "grep  --color=auto"
+abbr -a fgrep --position command "fgrep --color=auto"
+abbr -a egrep --position command "egrep --color=auto"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Combinations.
+
+function mkcd -d "Make a directory and CD into it"
+    mkdir "$argv" && cd "$argv"
+end
+
+function cdls -d "CD into a directory and list its contents"
+    cd "$argv" && ls
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# eza.
+
+function ls --wraps eza
+    eza --all --icons=auto --sort=name --group-directories-first $argv
+end
+
+function lsl --wraps eza
+    eza --all --icons=auto --sort=name --group-directories-first --grid --long \
+        --git --header $argv
+end
