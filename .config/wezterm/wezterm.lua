1342a4361404f6785690501efd20e2518cb90faf
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-02-14 19:58:23 -0800

    🎉 feat: Bootstrapping

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+-- wezterm API
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Macchiato'
+config.enable_tab_bar = false
+
+config.default_prog = { '/usr/bin/fish' }
+
+config.font = wezterm.font {
+	family = 'FiraCode Nerd Font',
+	harfbuzz_features = { 'cv02', 'cv06', 'ss01', 'cv14', 'onum', 'ss04', 'cv18', 'cv31', 'cv30', 'cv25', 'cv26', 'cv32', 'ss06', 'ss07' },
+}
+
+return config
