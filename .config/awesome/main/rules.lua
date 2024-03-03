bd1a1a6688eaa59f6a3e28182dc36b5992e223a7
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-03-03 14:27:42 -0800

    💄 style(awesome): Remove titlebars

--- .config/awesome/main/rules.lua
+++ .config/awesome/main/rules.lua
@@ -22,7 +22,7 @@ ruled.client.connect_signal("request::rules", function()
 			type = { "normal", "dialog" },
 		},
 		properties = {
-			titlebars_enabled = true,
+			titlebars_enabled = false,
 		},
 	})
 
