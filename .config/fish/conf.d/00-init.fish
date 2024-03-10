6b6013ac2c9a0543bd84caca39fa8aaaf61bac78
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-03-09 20:06:38 -0800

    ✨ perf(xdg): Add XDG dir global vars

--- .config/fish/conf.d/00-init.fish
+++ .config/fish/conf.d/00-init.fish
@@ -3,6 +3,12 @@
 # Base path.
 set -Ux fish_user_paths "$HOME/.local/bin"
 
+# Config files.
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
+set -Ux XDG_CACHE_HOME "$HOME/.cache"
+set -Ux XDG_DATA_HOME "$HOME/.local/share"
+set -Ux XDG_STATE_HOME "$HOME/.local/state"
+
 # Projects dir.
 set -gx HACK "$HOME/hackin"
 
