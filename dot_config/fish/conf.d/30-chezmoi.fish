403d1b0d17d10a833064f7b65acaa86b67ee8972
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-04-18 18:05:00 -0700

    Update .config/fish/conf.d/30-chezmoi.fish

--- dot_config/fish/conf.d/30-chezmoi.fish
+++ dot_config/fish/conf.d/30-chezmoi.fish
@@ -4,3 +4,4 @@ abbr -a cz --position command chezmoi
 abbr -a czap --position command "chezmoi apply"
 abbr -a cza --position command "chezmoi add"
 abbr -a czc --position command "chezmoi cd"
+abbr -a cze --position command "chezmoi edit"
