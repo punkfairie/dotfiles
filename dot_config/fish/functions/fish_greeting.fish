cb687f904742add74a1dc7360ea1b4c6b85b029e
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-04-21 13:51:00 -0700

    Update .config/fish/functions/fish_greeting.fish

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,5 +1,19 @@
 #!/usr/bin/env fish
 
 function fish_greeting
-    fortune | cowsay | lolcat
+    set -f cmd
+
+    if command -v fortune &>/dev/null
+        set -a cmd fortune
+    end
+
+    if command -v cowsay &>/dev/null
+        set -a cmd cowsay
+    end
+
+    if command -v lolcat &>/dev/null
+        set -a cmd "lolcat -t"
+    end
+
+    fish -c (string join ' | ' $cmd)
 end
