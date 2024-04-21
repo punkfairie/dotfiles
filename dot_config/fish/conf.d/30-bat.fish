4cb4bb57cc242fecb60048635d0728368dfcac2a
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-04-21 14:29:37 -0700

    Update .config/fish/conf.d/30-bat.fish

--- dot_config/fish/conf.d/30-bat.fish
+++ dot_config/fish/conf.d/30-bat.fish
@@ -1,14 +1,17 @@
 #!/usr/bin/env fish
 
 set -gx BATDIFF_USE_DELTA true
+set -g batcmd
 
 function cat --wraps bat
     if command -v bat &>/dev/null
+        set batcmd bat
         bat $argv
     else if command -v batcat &>/dev/null
+        set batcmd batcat
         batcat $argv
     end
 end
 
-abbr -a B --position anywhere --set-cursor "% | bat"
-abbr -a --position anywhere -- -h "-h 2>&1 | bat --plain --language=help"
+abbr -a B --position anywhere --set-cursor "% | $batcmd"
+abbr -a --position anywhere -- -h "-h 2>&1 | $batcmd --plain --language=help"
