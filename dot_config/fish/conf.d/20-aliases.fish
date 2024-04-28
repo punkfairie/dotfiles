b64f47f965654b2b9c8ea86249ea1ec533d22031
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-04-27 19:24:05 -0700

    Update .config/fish/conf.d/20-aliases.fish

--- dot_config/fish/conf.d/20-aliases.fish
+++ dot_config/fish/conf.d/20-aliases.fish
@@ -77,12 +77,3 @@ end
 function cdls -d "CD into a directory and list its contents"
     cd "$argv" && ls
 end
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Check battery.
-
-function batt -d "Check battery percentage"
-    upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
-        | grep -E "state|time to|percentage"
-end
