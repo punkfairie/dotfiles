71cedb1ca14316c6f4abce371d0ff35eea1a1d8f
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-06-10 17:48:49 -0700

    Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/plugins/conform.lua Update .config/nvim/lua/plugins/mason.lua Update .config/nvim/lua/plugins/nvim-lint.lua Update .config/nvim/lua/plugins/nvim-lspconfig.lua Update .config/nvim/lua/plugins/nvim-treesitter.lua Remove .config/nvim/lua/plugins/refactoring.lua Update tags

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -3,7 +3,6 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
-        sql = { "sqlfluff" },
         yaml = { "yamllint" },
         bash = { "shellcheck" },
         sh = { "shellcheck" },
