85ee314147994742e3d482d3b7b8837dcf016b61
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-07-29 17:23:35 -0700

    Update .config/i3/launch.sh Update .config/packages/archgirlie Update tags

--- dot_config/i3/launch.sh
+++ dot_config/i3/launch.sh
@@ -2,3 +2,6 @@
 
 "$HOME/.config/fehbg/i3"
 picom --config "$HOME/.config/picom/i3" &
+
+"$HOME/.config/polybar/colorblocks/scripts/pywal.sh" "$HOME/data/photos/wallpaper/cloud.png"
+"$HOME/.config/polybar/launch.sh --colorblocks"
