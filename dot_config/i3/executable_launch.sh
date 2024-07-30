f66543395e19f2219640cbd118909b0652acd123
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-07-29 17:27:07 -0700

    Update .config/i3/launch.sh

--- dot_config/i3/executable_launch.sh
+++ dot_config/i3/executable_launch.sh
@@ -3,5 +3,4 @@
 "$HOME/.config/fehbg/i3"
 picom --config "$HOME/.config/picom/i3" &
 
-"$HOME/.config/polybar/colorblocks/scripts/pywal.sh" "$HOME/data/photos/wallpaper/cloud.png"
 "$HOME/.config/polybar/launch.sh --colorblocks"
