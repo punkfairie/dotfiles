3e0d14aabd87bd253bc6b4022eec61c8c6af27be
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-08-04 15:26:00 -0700

    Update .config/polybar/colorblocks/launch.sh

--- dot_config/polybar/colorblocks/executable_launch.sh
+++ dot_config/polybar/colorblocks/executable_launch.sh
@@ -15,9 +15,10 @@ sleep 10
 # Launch the bar
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		echo "$m" >>"$HOME/.scratch"
 		MONITOR=$m polybar -q main -c "$DIR"/config.ini &
 	done
+
+	MONITOR="DVI-D-0" polybar -q main -c "$DIR"/config.ini &
 else
 	polybar -q main -c "$DIR"/config.ini &
 fi
