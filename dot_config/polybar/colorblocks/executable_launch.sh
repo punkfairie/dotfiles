e7f1db6f88e58406eec773c8f6084d3011b60ab3
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-08-04 15:27:44 -0700

    Update .config/polybar/colorblocks/launch.sh

--- dot_config/polybar/colorblocks/executable_launch.sh
+++ dot_config/polybar/colorblocks/executable_launch.sh
@@ -10,8 +10,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-sleep 10
-
 # Launch the bar
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
