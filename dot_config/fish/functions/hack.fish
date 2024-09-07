e9e1c105efff04d349771cf98d4589966c0bd478
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-09-06 18:42:57 -0700

    Update .config/fish/functions/hack.fish

--- dot_config/fish/functions/hack.fish
+++ dot_config/fish/functions/hack.fish
@@ -2,7 +2,7 @@
 
 function hack --description "Select a GH repo, [clone it], enter its directory" \
     --argument-names repo
-    if ! set -q repo
+    if string length -q "$repo"
         set -f repos \
             $(gh repo list --no-archived --json name,description --jq '.[] | "\(.name) \u001b[1;30m\(.description)\u001b[0m"')
         set -f repo $(gum filter --height 10 $repos | string split ' ')[1]
