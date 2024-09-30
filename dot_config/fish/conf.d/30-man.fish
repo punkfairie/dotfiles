27a8517fdd33130e2197377944cfdec0819a8772
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-09-29 21:39:00 -0700

    Update .config/fish/conf.d/30-man.fish Update .config/packages/archgirlie

--- dot_config/fish/conf.d/30-man.fish
+++ dot_config/fish/conf.d/30-man.fish
@@ -1,5 +1,11 @@
 #!/usr/bin/env fish
 
-if command -v most &>/dev/null
-    set -gx MANPAGER most
+if command -v bat &>/dev/null
+    set batcmd bat
+else if command -v batcat &>/dev/null
+    set batcmd batcat
+end
+
+if set -q batcmd
+    set -gx MANPAGER "sh -c 'col -bx | $batcmd -l man -p'"
 end
