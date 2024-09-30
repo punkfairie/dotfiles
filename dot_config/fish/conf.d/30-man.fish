67f363abc986430b8d0ed744d705a196f5f3fb15
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-09-29 21:41:35 -0700

    Update .config/fish/conf.d/30-man.fish

--- dot_config/fish/conf.d/30-man.fish
+++ dot_config/fish/conf.d/30-man.fish
@@ -1,11 +1,5 @@
 #!/usr/bin/env fish
 
-if command -v bat &>/dev/null
-    set batcmd bat
-else if command -v batcat &>/dev/null
-    set batcmd batcat
-end
-
-if set -q batcmd
-    set -gx MANPAGER "sh -c 'col -bx | $batcmd -l man -p'"
+if command -v most &>/dev/null
+    set -gx MANPAGER "sh -c 'col -bx | most'"
 end
