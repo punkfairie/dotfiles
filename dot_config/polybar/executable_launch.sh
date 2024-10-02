cf917a845bc3a1bd1f88dc5976434ae1a1b76160
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-01 19:29:57 -0700

    Update .config/packages/arch-packages.fish Update .config/polybar/launch.sh Update tags

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,76 +1,83 @@
 #!/usr/bin/env bash
 
 dir="$HOME/.config/polybar"
-themes=(`ls --hide="launch.sh" $dir`)
+# themes=(`ls --hide="launch.sh" $dir`)
 
-launch_bar() {
-	# Terminate already running bar instances
-	killall -q polybar
+launch_bar()
+{
+  # Terminate already running bar instances
+  killall -q polybar
 
-	# Wait until the processes have been shut down
-	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+  # Wait until the processes have been shut down
+  while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-	# Launch the bar
-	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
-		polybar -q top -c "$dir/$style/config.ini" &
-		polybar -q bottom -c "$dir/$style/config.ini" &
-	elif [[ "$style" == "pwidgets" ]]; then
-		bash "$dir"/pwidgets/launch.sh --main
-	else
-		polybar -q main -c "$dir/$style/config.ini" &	
-	fi
+  # Launch the bar
+  if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
+    polybar -q top -c "$dir/$style/config.ini" &
+    polybar -q bottom -c "$dir/$style/config.ini" &
+  elif [[ "$style" == "pwidgets" ]]; then
+    bash "$dir"/pwidgets/launch.sh --main
+  else
+    if type "xrandr" &>/dev/null; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -q main -c "$dir/$style/config.ini" &
+      done
+    else
+      polybar -q main -c "$dir/$style/config.ini" &
+    fi
+  fi
 }
 
 if [[ "$1" == "--material" ]]; then
-	style="material"
-	launch_bar
+  style="material"
+  launch_bar
 
 elif [[ "$1" == "--shades" ]]; then
-	style="shades"
-	launch_bar
+  style="shades"
+  launch_bar
 
 elif [[ "$1" == "--hack" ]]; then
-	style="hack"
-	launch_bar
+  style="hack"
+  launch_bar
 
 elif [[ "$1" == "--docky" ]]; then
-	style="docky"
-	launch_bar
+  style="docky"
+  launch_bar
 
 elif [[ "$1" == "--cuts" ]]; then
-	style="cuts"
-	launch_bar
+  style="cuts"
+  launch_bar
 
 elif [[ "$1" == "--shapes" ]]; then
-	style="shapes"
-	launch_bar
+  style="shapes"
+  launch_bar
 
 elif [[ "$1" == "--grayblocks" ]]; then
-	style="grayblocks"
-	launch_bar
+  style="grayblocks"
+  launch_bar
 
 elif [[ "$1" == "--blocks" ]]; then
-	style="blocks"
-	launch_bar
+  style="blocks"
+  launch_bar
 
 elif [[ "$1" == "--colorblocks" ]]; then
-	style="colorblocks"
-	launch_bar
+  style="colorblocks"
+  launch_bar
 
 elif [[ "$1" == "--forest" ]]; then
-	style="forest"
-	launch_bar
+  style="forest"
+  launch_bar
 
 elif [[ "$1" == "--pwidgets" ]]; then
-	style="pwidgets"
-	launch_bar
+  style="pwidgets"
+  launch_bar
 
 elif [[ "$1" == "--panels" ]]; then
-	style="panels"
-	launch_bar
+  style="panels"
+  launch_bar
 
 else
-	cat <<- EOF
+  cat <<-EOF
 	Usage : launch.sh --theme
 		
 	Available Themes :
