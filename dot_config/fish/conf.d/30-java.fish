63d922b82ae6ec1d80269a693cb9d2e4ae798662
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-04 21:57:53 -0700

    Update .config/fish/conf.d/30-java.fish

--- dot_config/fish/conf.d/30-java.fish
+++ dot_config/fish/conf.d/30-java.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-set -Ux _JAVA_OPTIONS "-Djavafx.cachedir=$XDG_CACHE_HOME/openjfx -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java"
+set -Ux _JAVA_OPTIONS -Djavafx.cachedir="$XDG_CACHE_HOME"/openjfx -Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
