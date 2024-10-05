599d8f83d9aada1f1f37d7aa8f9654a8e2e226bb
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-04 22:09:58 -0700

    Update .config/fish/conf.d/30-go.fish

--- dot_config/fish/conf.d/30-go.fish
+++ dot_config/fish/conf.d/30-go.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
+set -Ux GOPATH "$XDG_DATA_HOME/go"
+set -Ux GOMODCACHE "$XDG_CACHE_HOME/go/mod"
+
 if command -v go &>/dev/null
-    fish_add_path "$HOME/go/bin"
+    fish_add_path "$XDG_DATA_HOME/go/bin"
 end
