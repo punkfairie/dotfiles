156e11a40441e8a715d1b0885335a321323e6009
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-04 22:30:27 -0700

    Update .config/fish/conf.d/30-python.fish

--- dot_config/fish/conf.d/30-python.fish
+++ dot_config/fish/conf.d/30-python.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+set -Ux PYTHON_HISTORY "$XDG_STATE_HOME/python/history"
+
 if command -v brew &>/dev/null
     fish_add_path "$(brew --prefix)/opt/python@3.12/libexec/bin"
 end
