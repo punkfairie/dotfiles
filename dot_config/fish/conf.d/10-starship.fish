0eaffd3a962ee3ea73003aa686050adee09668a0
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-05 14:49:18 -0700

    Update .config/fish/conf.d/10-starship.fish

--- dot_config/fish/conf.d/10-starship.fish
+++ dot_config/fish/conf.d/10-starship.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+set -Ux STARSHIP_CONFIG "$XDG_CONFIG_HOME/starship.toml"
+
 if command -v starship &>/dev/null
     function starship_transient_prompt_func
         starship module character
