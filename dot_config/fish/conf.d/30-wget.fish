ed5a3f21cd47f8c566c24e09e22e7f1e3b03b395
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-05 10:32:15 -0700

    Update .config/fish/conf.d/30-wget.fish

--- dot_config/fish/conf.d/30-wget.fish
+++ dot_config/fish/conf.d/30-wget.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
 
+set -Ux WGETRC "$XDG_CONFIG_HOME/wgetrc"
+
 if command -v wget &>/dev/null
-    set -gx WGETRC "$XDG_CONFIG_HOME/wgetrc"
 
     function wget --wraps wget
         command wget --hsts-file="$XDG_CACHE_HOME/wget-hsts" $argv
