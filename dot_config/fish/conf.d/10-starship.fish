4b2f52443ae880d664c4f701fc49b5eb9f019134
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-05 14:49:49 -0700

    Update .config/fish/conf.d/10-starship.fish

--- dot_config/fish/conf.d/10-starship.fish
+++ dot_config/fish/conf.d/10-starship.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 set -Ux STARSHIP_CONFIG "$XDG_CONFIG_HOME/starship.toml"
+set -Ux STARSHIP_CACHE "$XDG_CACHE_HOME/starship"
 
 if command -v starship &>/dev/null
     function starship_transient_prompt_func
