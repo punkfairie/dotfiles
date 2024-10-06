9201d63bb3b75f759ac42a187b1cd3bbfe567e03
punkfairie <23287005+punkfairie@users.noreply.github.com>
2024-10-05 17:03:19 -0700

    Add .config/nvim/lua/plugins/zeavim.lua Update .config/packages/archgirlie

---
+++ dot_config/nvim/lua/plugins/zeavim.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "KabbAmine/zeavim.vim",
+    cmd = { "Zeavim", "ZeavimV", "Zeavim!", "Docset" },
+    keys = {
+      {
+        "<leader>z",
+        "<Plug>Zeavim",
+        desc = "Search word under cursor in Zeal",
+      },
+      {
+        "<leader>z",
+        "<Plug>ZVVisSelection",
+        mode = "v",
+        desc = "Search visual selection in Zeal",
+      },
+      { "gz", "<Plug>ZVOperator", desc = "Search motion/text-object in Zeal" },
+      {
+        "<leader><leader>z",
+        "<Plug>ZVKeyDocset",
+        desc = "Search a specific docset in Zeal",
+      },
+    },
+  },
+}
